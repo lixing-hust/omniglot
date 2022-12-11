2cc1296cd3e66b30f29c9cd275fe0e87d5c84b36
li xing <1291729176@qq.com>
2022-12-11 12:14:45 +0800

    加点注释

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ def data_generator(N,s,q,dir):
     task=[]
     spt_set=[]
     qry_set=[]
-    while len(random_nums)<N:
+    while len(random_nums)<N:#找N个不重复随机数
         a=random.randint(1,659)
         if a not in random_nums:
             random_nums.append(a)
     language=os.listdir(dir)
     character_num=[]
-    for l in language:
+    for l in language:#计算每个字符集的数量
         character_num.append(len(os.listdir(dir+l)))
-    for i in range(len(character_num)-1):
+    for i in range(len(character_num)-1):#将每种语言之前的所有字符含量加到这个语言上，表示到这个语言为止有多少种字符
         character_num[i+1]=character_num[i]+character_num[i+1]
 
     for i,nums in enumerate(random_nums):
-        for index,ch_num in enumerate(character_num):
+        for index,ch_num in enumerate(character_num):#找随机数对应的字符
             if nums<=ch_num:
                 break
         language_dir=dir+language[index]+'/'
@@ -43,7 +43,7 @@ def data_generator(N,s,q,dir):
         else :
             character_dir=language_dir+'character'+str(character_index)+'/'
         images_dir_list=os.listdir(character_dir)
-        for cnt in range(s+q):
+        for cnt in range(s+q):#字符前s个图片加入到spt中，第s+1到q的图片加入到qry中
             image_dir=character_dir+images_dir_list[cnt]
             image=Image.open(image_dir)
             if cnt < s:
@@ -56,7 +56,7 @@ def data_generator(N,s,q,dir):
 
 
 
-def data_init(N,s,q):
+def data_init(N,s,q):#给数据加label
     task=data_generator(N,s,q,'images_evaluation/images_evaluation/')
     train_data=task[0]
     test_data=task[1]
